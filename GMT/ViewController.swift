a840935e7cb0d8a99435408989c4ecfd47067979
James O'Brien <jaimzo@gmail.com>
2016-05-20 20:10:37 +0100

    Initial Commit

---
+++ GMT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GMT
+//
+//  Created by James O'Brien on 20/05/2016.
+//  Copyright © 2016 James O'Brien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
